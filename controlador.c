b718c539fd2e89b5da8c3083cc2f18b19d04eae3
AgusC2310 <agcasas@itba.edu.ar>
2023-02-06 00:01:40 -0300

    a

--- controlador.c
+++ controlador.c
@@ -2,15 +2,29 @@
 #include <stdint.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include "shapes.h"
 
+/************************************************************PROTOTYPES******************************************************************************/
 void print_mat (uint8_t mat[16][16]);
 void clear_mat (uint8_t mat[16][16]);
+void initializeShip (ship_t * sh);
+void copyCustomShip (ship_t * sh);
+void introduceShip (ship_t *sh, uint8_t mat[16][16]);
+
+/*****************************************************************************************************************************************************/
+uint8_t customShip[6][6]= {{0,1,0,0,0,0},{1,1,1,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0}};
 
 int main (void)
 {
-	uint8_t matriz[16][16];
-	clear_mat(matriz);
-	print_mat(matriz);
+	uint8_t matrix[16][16];
+	ship_t ship;
+	initializeShip(&ship);
+	ship.posx=4;
+	ship.posy= 4;
+	
+	clear_mat(matrix);
+	introduceShip(&ship, matrix);
+	print_mat(matrix);
 	return 0;
 
 }
@@ -39,4 +53,30 @@ void print_mat (uint8_t mat[16][16])
 		printf("\n");
 	}
 	printf("\n");
+}
+
+void initializeShip (ship_t * sh){
+	sh->posx = 0;
+	sh->posy = 0;
+	copyCustomShip(sh);
+	sh->velx =0;
+	sh->vely = 0;
+}
+
+void copyCustomShip (ship_t * sh){
+	for(int i=0; i<SHIPSIZE; i++)
+	{
+		for(int j=0; j<SHIPSIZE; j++)
+		{
+			sh->shape[i][j]= customShip[i][j];
+		}
+	}
+}
+
+void introduceShip (ship_t *sh, uint8_t mat[16][16]){
+	for(int i=0; i<SHIPSIZE; i++){
+		for(int j=0; j< SHIPSIZE; j++){
+			mat[i+sh->posx][j+sh->posy]= sh->shape[i][j];
+		}
+	}
 }
--- shapes.h
+++ shapes.h
@@ -1,15 +1,15 @@
 #ifndef SHAPES_H
 #define SHAPES_H
-#define SHIPSIZE 3
+#define SHIPSIZE 6
 #include <stdint.h>
 
+ 
 typedef struct {
-    uint8_t size;
     uint8_t shape[SHIPSIZE][SHIPSIZE];
-    uint16_t posx;
-    uint16_t posy;
-    uint16_t velx;
-    uint16_t vely;
+    uint16_t posx; //x position of the top left element of the matrix
+    uint16_t posy;//y position of the top left element of the matrix
+    uint16_t velx; //x velocity of the top left element of the matrix
+    uint16_t vely;  //y velocity of the top left element of the matrix
     
 } ship_t;
 
